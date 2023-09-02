3a4d378567984ed565690a33f11801bf9818a6d6
Mark Lamay <lemay@bu.edu>
2023-09-02 16:27:27 -0400

    clean up

--- editor.ts
+++ editor.ts
@@ -1,42 +1,6 @@
 import {EditorView, basicSetup} from "codemirror"
-//import {javascript} from "@codemirror/lang-javascript"
-//import {syntaxTree} from "@codemirror/language"
 import type {Diagnostic} from "@codemirror/lint"
 import {linter} from "@codemirror/lint"
-
-
-
-
-
-/* import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
-
-export const EXAMPLELanguage = LRLanguage.define({
-  parser: parser.configure({
-    props: [
-      foldNodeProp.add({
-        Application: foldInside
-      }),
-      styleTags({
-        Identifier: t.variableName,
-        LineComment: t.lineComment,
-        "( )": t.paren
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {line: "--"}
-  }
-})
-
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage) 
-}
-*/
-
-
-
 
 
 const regexpLinter = linter(view => {
